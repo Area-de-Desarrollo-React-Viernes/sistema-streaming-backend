ce51620451cc7b5af09b3dae6306e3979e9d1522
lventocillap <1507430@senati.pe>
2025-04-19 15:53:11 -0500

    refactor: :recycle: Validate Google user
    Google user validation before registering or logging in.

--- src/auth/application/use-case/CreateUserGoogleUseCase.ts
+++ src/auth/application/use-case/CreateUserGoogleUseCase.ts
@@ -1,11 +1,16 @@
 import { AuthGoogleService } from "../../domain/service/AuthGoogleService";
+import { UserAuthGoogleService } from "../../infrastructure/service/UserAuthGoogleServie";
 
 export class CreateUserGoogleUseCase {
     constructor(
         private googleRepository: AuthGoogleService
-    ){}
+    ) { }
     async run(token: string): Promise<string> {
-        const tokenJWT = await this.googleRepository.createUserGoogle(token);
+        const user = await this.googleRepository.findUserGoogle(token);
+        if (!user) {
+            throw new UserAuthGoogleService;
+        }
+        const tokenJWT = await this.googleRepository.createUserGoogle(user);
         return tokenJWT;
     }
 }
--- src/auth/domain/service/AuthGoogleService.ts
+++ src/auth/domain/service/AuthGoogleService.ts
@@ -3,6 +3,6 @@ import { UserGoogle } from "../entities/UserGoogle";
 
 export interface AuthGoogleService {
     findUserGoogle(token: string): Promise<UserGoogle | null>
-    createUserGoogle(token: string): Promise<string>
-    loginUserGoogle(token: string): Promise<string>
+    createUserGoogle(user: UserGoogle): Promise<string>
+    loginUserGoogle(user: UserGoogle): Promise<string>
 }
