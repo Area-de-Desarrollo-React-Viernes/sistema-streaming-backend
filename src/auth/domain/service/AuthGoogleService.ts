b796120bd7aa12a329498858c29c62ccef79644f
lventocillap <1507430@senati.pe>
2025-04-19 15:18:52 -0500

    feat: :sparkles: Search Google User
    search for the Google user using the validation token

--- src/auth/domain/service/AuthGoogleService.ts
+++ src/auth/domain/service/AuthGoogleService.ts
@@ -1,6 +1,8 @@
 import { User } from "../entities/User";
+import { UserGoogle } from "../entities/UserGoogle";
 
 export interface AuthGoogleService {
+    findUserGoogle(token: string): Promise<UserGoogle | null>
     createUserGoogle(token: string): Promise<string>
     loginUserGoogle(token: string): Promise<string>
 }
